fe1e81c1b33652253ec2906123197609efca7c97
skymarkos7 <marcosrealbmata@gmail.com>
2022-01-30 20:50:30 -0300

    icone histórico de vendas

--- paginas/historico_de_vendas.php
+++ paginas/historico_de_vendas.php
@@ -15,7 +15,7 @@ include_once("parte_de_cima.php");
             <div class="page-title-heading">
                 <div class="page-title-icon">
                     <i class="pe-7s-car icon-gradient bg-mean-fruit">
-                        <img class="ilustracao_funcao" src="../img/icone/painel3.png" alt="painel de controle">
+                        <img class="ilustracao_funcao" src="../img/icone/relatorio-de-negocios.png" alt="painel de controle">
                     </i>
                 </div>
                 <div>Relatórios
