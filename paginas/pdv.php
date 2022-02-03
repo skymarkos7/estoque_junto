9c978cd4f0faccfdc96fd4a2255b8eb2e8ee57a1
skymarkos7 <marcosrealbmata@gmail.com>
2022-02-02 21:58:04 -0300

    inserindo hora com JS

--- paginas/pdv.php
+++ paginas/pdv.php
@@ -39,12 +39,49 @@ include_once("parte_de_cima.php");
 
         </div>
 
+
+
+
+
+
+
+        <script>
+// Obtém a data/hora atual
+var data = new Date();
+
+// Guarda cada pedaço em uma variável
+var dia     = data.getDate();           // 1-31
+var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+var ano2    = data.getYear();           // 2 dígitos
+var ano4    = data.getFullYear();       // 4 dígitos
+var hora    = data.getHours();          // 0-23
+var min     = data.getMinutes();        // 0-59
+var seg     = data.getSeconds();        // 0-59
+var mseg    = data.getMilliseconds();   // 0-999
+var tz      = data.getTimezoneOffset(); // em minutos
+
+// Formata a data e a hora (note o mês + 1)
+var str_data = dia + '/' + (mes+1) + '/' + ano4;
+var str_hora = hora + ':' + min + ':' + seg;
+
+
+
+
+</script>
+
+
+
+
+
+
+
+
         <div class="horario_pdv">
-            <p>12:59:47</p>
+            <p id="hora"><script>window.document.write(str_hora);</script></p>
         </div>
 
     </div>
-
 
 
     <div class="parte_busca_pdv">
