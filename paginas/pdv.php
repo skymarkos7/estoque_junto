8f58f40141d626da5ce76a9038e9b849f3b75b0e
Marcos Lourenco <marcosrealbmata@gmail.com>
2022-02-02 22:47:41 -0300

    MELHORANDO A APRESENTACAO DA HORA

--- paginas/pdv.php
+++ paginas/pdv.php
@@ -40,35 +40,47 @@ include_once("parte_de_cima.php");
         </div>
 
 
+<style>
+#hora{
+    text-align: center;
+    color: #0f0;
+    font-weight: bold;
+    font-size: 25px;
+    width: 130px;
+    background-color: black;
+}
 
+</style>
 
 
 
 
         <script>
-// Obtém a data/hora atual
-var data = new Date();
+		
+        function relogio(){
+            var data=new Date();
+            var hor=data.getHours();
+            var min=data.getMinutes();
+            var seg=data.getSeconds();
+            
+            if(hor < 10){
+                hor="0"+hor;
+            }
+            if(min < 10){
+                min="0"+min;
+            }
+            if(seg < 10){
+                seg="0"+seg;
+            }
+            
+            var horas=hor + ":" + min + ":" + seg;
+            
+            document.getElementById("hora").value=horas;
+        }
 
-// Guarda cada pedaço em uma variável
-var dia     = data.getDate();           // 1-31
-var dia_sem = data.getDay();            // 0-6 (zero=domingo)
-var mes     = data.getMonth();          // 0-11 (zero=janeiro)
-var ano2    = data.getYear();           // 2 dígitos
-var ano4    = data.getFullYear();       // 4 dígitos
-var hora    = data.getHours();          // 0-23
-var min     = data.getMinutes();        // 0-59
-var seg     = data.getSeconds();        // 0-59
-var mseg    = data.getMilliseconds();   // 0-999
-var tz      = data.getTimezoneOffset(); // em minutos
+        var timer=setInterval(relogio,1000);
 
-// Formata a data e a hora (note o mês + 1)
-var str_data = dia + '/' + (mes+1) + '/' + ano4;
-var str_hora = hora + ':' + min + ':' + seg;
-
-
-
-
-</script>
+    </script>
 
 
 
@@ -78,7 +90,7 @@ var str_hora = hora + ':' + min + ':' + seg;
 
 
         <div class="horario_pdv">
-            <p id="hora"><script>window.document.write(str_hora);</script></p>
+            <input type="text" id="hora">
         </div>
 
     </div>
