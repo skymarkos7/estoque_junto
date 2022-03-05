19af6a7ced963cf7a86f4a0948fda86fac36d8cd
Guilherme Lourenço <ograodetrigo@gmail.com>
2022-03-05 07:24:21 -0300

    CAMPO DE PESQUISA

--- banco/listar_produtos.php
+++ banco/listar_produtos.php
@@ -10,9 +10,7 @@ $resultado_produto = mysqli_query($conn, $result_produto);
 
 //Verificar se encontrou resultado na tabela "usuarios"
 if (($resultado_produto) and ($resultado_produto->num_rows != 0)) {
-        while ($row_produto = mysqli_fetch_assoc($resultado_produto)) {
-
-                
+        while ($row_produto = mysqli_fetch_assoc($resultado_produto)) {                
                       
                         echo "<div id='quadrado_produto' class='quadrado main-card mb-3 card'> NOME POPULAR: &nbsp;" . $row_produto['nome_popular'] . "<br>";
                         echo "NOME TÉCNICO: &nbsp;" . $row_produto['nome_tecnico'] . "<br>";
@@ -23,10 +21,16 @@ if (($resultado_produto) and ($resultado_produto->num_rows != 0)) {
                         echo "QUANTIDADE: &nbsp;" . $row_produto['quantidade'] . "<br>";
                         echo "LOCAL: &nbsp;" . $row_produto['lugar'] . "<br>";
                         echo "POSSUI NOTA?: &nbsp;" . $row_produto['nota'] . "</div>";
-                    
-                
+                                    
 
         }
 } else {
         echo "Nenhum produto encontrado";
 }
+
+
+
+
+
+
+?>
